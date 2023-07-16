765da0fe48d8093806c59720e2659478d9445a7f
TsuyoshiUsugi <qesulive@gmail.com>
2023-07-16 21:28:14 +0900

    [update] コントローラー対応中

---
+++ Assets/Scripts/BarHockey/ControllerInputManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TsuyoshiLibrary
+{
+    /// <summary>
+    /// 各プレイヤーのコントローラーのLRスティック入力を受け取るマネージャ
+    /// </summary>
+    public class ControllerInputManager : MonoBehaviour
+    {
+        [SerializeField] string _p1LeftStickInputName;
+        [SerializeField] string _p1RightStickInputName;
+
+        private void Update()
+        {
+            
+        }
+    }
+}
