1635b6d99e075114d63f8efc7e0d6188ff194469
TsuyoshiUsugi <qesulive@gmail.com>
2023-07-17 21:35:20 +0900

    [update] とりあえずeffect途中まで

--- Assets/Scripts/BarHockey/EffectManager.cs
+++ Assets/Scripts/BarHockey/EffectManager.cs
@@ -6,5 +6,23 @@ namespace TsuyoshiLibrary
 {
     public class EffectManager : MonoBehaviour
     {
+        [SerializeField] ParticleSystem _hitWallParticle;
+        [SerializeField] ParticleSystem _goalParticle;
+
+        public void GenerateParticle(Vector3 pos, ParticleType type)
+        {
+            switch (type)
+            {
+                case ParticleType.Hit:
+
+                    break;
+            }
+        }
+
+        public enum ParticleType
+        {
+            Hit,
+            Goal,
+        }
     }
 }
