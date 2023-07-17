2830d35619171fd3d4eee9b93841be479a1f1bba
TsuyoshiUsugi <qesulive@gmail.com>
2023-07-17 22:22:11 +0900

    [update ]フレームスキップを導入、しかし効果がない

--- Assets/Scripts/BarHockey/TweenMovePack.cs
+++ Assets/Scripts/BarHockey/TweenMovePack.cs
@@ -17,15 +17,17 @@ namespace TsuyoshiLibrary
     {
         [Header("設定値")]
         [SerializeField, Tooltip("移動にかかる時間")] float _moveDuration = 10; 
+        [SerializeField, Tooltip("独自のイージングを使うか")] bool _customEase = false;
         [SerializeField, Tooltip("独自に作成するイージング")] AnimationCurve _curve;
-        [SerializeField] Ease _ease = Ease.InQuint;
-        [SerializeField] bool _customEase = false;
+        [SerializeField, Tooltip("元からあるイージング")] Ease _ease = Ease.InQuint;
+        [SerializeField, Tooltip("連続してHitした際に何フレームSkipするか")] float _skipFrame = 10; 
         [Header("確認用")]
         [SerializeField] Vector3 _dir = Vector3.zero;
         [SerializeField] Vector3 _targetPoint = Vector3.zero;
         RaycastHit _hit;
         Tween _currentTween = null;
         Ray _ray;
+        bool _isInSkipFrame = false;
 
         private void Start()
         {
@@ -44,9 +46,7 @@ namespace TsuyoshiLibrary
         /// </summary>
         void ShowReflectionPredictionPoint()
         {
-            
             Debug.DrawRay(_ray.origin, _ray.direction * 100, Color.blue);
-            
         }
 
         void RayCastToPoint()
@@ -55,9 +55,9 @@ namespace TsuyoshiLibrary
             var hits = Physics.RaycastAll(_ray);
             if (hits.Length > 1)
             {
-                for (int i = 0; i < hits.Length; i++)   //PlayerとPackオブジェクトを目標地点にしてしまうと途中でパックが止まってしまうので除外
+                for (int i = 0; i < hits.Length; i++)   //PlayerとPackとBarオブジェクトを目標地点にしてしまうと途中でパックが止まってしまうので除外
                 {
-                    if (hits[i].transform.gameObject.tag != "Player" || hits[i].transform.gameObject.tag == "Pack")
+                    if (hits[i].transform.gameObject.tag != "Player" || hits[i].transform.gameObject.tag == "Pack" || hits[i].transform.gameObject.tag == "Bar")
                     {
                         _hit = hits[i];
                     }
@@ -69,12 +69,25 @@ namespace TsuyoshiLibrary
             }
         }
 
+        private IEnumerator WaitForFrames()
+        {
+            _isInSkipFrame = true;
+            for (int i = 0; i < _skipFrame; i++)
+            {
+                yield return null; // 1フレーム待機
+            }
+            _isInSkipFrame = false;
+        }
+
         void TweenMove(Vector3 point)
         {
+            if (_isInSkipFrame) return;
+
+            StartCoroutine(nameof(WaitForFrames));
+
             if (_customEase)
             {
                 _currentTween = transform.DOMove(point, _moveDuration).SetEase(_curve);
-                
             }
             else
             {
@@ -82,13 +95,12 @@ namespace TsuyoshiLibrary
             }
             _targetPoint = point;
         }
-
+        
         private void OnCollisionEnter(Collision collision)
         {
-            
+            Debug.Log(collision.gameObject.tag);
             if (collision.gameObject.tag == "Goal")  //ゴールに着いたら中心へ瞬間移動
             {
-                Debug.Log("Goal");
                 _currentTween.Kill();
                 transform.position = Vector3.zero;
                 _dir = GetRandomDirection();
@@ -97,15 +109,13 @@ namespace TsuyoshiLibrary
             }
             else if (collision.gameObject.tag == "Player")  //playerにあたったら中心点の差分を進行方向に
             {
-                Debug.Log("Player");
                 _dir = (transform.position - collision.gameObject.transform.position).normalized;
                 _dir.y = 0;
                 RayCastToPoint();
                 TweenMove(_hit.point);
             }
-            else　//その他の物体(今の所カベのみ)にあたったら入射角と法線を計算して反射角を割り出す
+            else　if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Bar")   //壁とプレイヤーのバーにあたったら法線を取得して反射角を割り出して跳ね返る
             {
-                Debug.Log("Other");
                 var inDirection = _dir;
                 var inNormal = collision.contacts[0].normal;
                 float dot = Vector3.Dot(inDirection, inNormal);
